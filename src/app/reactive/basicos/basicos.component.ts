581453de01d8900750c990c0ebab1952e3605358
Yaqi <yaqiye22@gmail.com_>
2023-03-23 19:31:40 +0100

    Sección 17 terminada. 23_03_2023 wip

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,9 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html'
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
+
+  /*
+  miFormulario: FormGroup = new FormGroup({
+
+    nombre     : new FormControl( 'RTX 4080ti' ),
+    precio     : new FormControl( 1500 ),
+    existencias: new FormControl( 5 )
+  })
+  */
+
+  miFormulario: FormGroup = this.fb.group({
+
+    nombre     : [ , [ Validators.required, 
+                         Validators.minLength( 3 ) ] ],
+    precio     : [ , [ Validators.required, 
+                        Validators.min( 0 ) ] ],
+    existencias: [ , [ Validators.required, 
+                        Validators.min( 0 ) ] ]
+
+  })
+
+  constructor( private fb: FormBuilder ){
+
+  }
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'RTX 4080ti',
+      precio: 1600
+    })
+  }
+
+  campoNoValido( campo: string ){
+
+    return this.miFormulario.controls[campo].errors &&
+           this.miFormulario.controls[campo].touched;
+
+  }
+
+  guardar() {
+
+    if( this.miFormulario.invalid ) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log( this.miFormulario.value );
+    this.miFormulario.reset();
+  }
 
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
+
 
 
 @NgModule({
@@ -15,7 +17,8 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ReactiveModule { }
